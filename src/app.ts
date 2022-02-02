ed4485edc83f079bdfe46ea862dae2ca74d11f7b
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-02-02 09:02:58 +0530

    feat: add abstact base component class

--- src/app.ts
+++ src/app.ts
@@ -108,6 +108,35 @@ const autoBind = (
     return adjDescriptor;
 }
 
+// Project Base Component
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+    
+    constructor(templateElementID: string, hostElementID: string, insertAtStart: boolean, newElementID?: string) {
+        this.templateElement = document.getElementById(templateElementID)! as HTMLTemplateElement;
+        this.hostElement = document.getElementById(hostElementID)! as T;
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild! as U;
+        if (newElementID !== undefined) {
+            this.element.id = newElementID;
+        }
+        this.attach(insertAtStart);
+    }
+
+    private attach(insertAtStart: boolean) {
+        if (insertAtStart === true) {
+            this.hostElement.insertAdjacentElement("afterbegin", this.element);
+        } else {
+            this.hostElement.insertAdjacentElement("beforeend", this.element);
+        }
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+}
+
 // ProjectList class
 class ProjectList {
     templateElement: HTMLTemplateElement;
