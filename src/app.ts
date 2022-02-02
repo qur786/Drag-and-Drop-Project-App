784a49cec6097d131ea947b1bc6d30ce0a4802ea
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-02-02 20:15:00 +0530

    refactor: refactor Project Input class with Base class Component

--- src/app.ts
+++ src/app.ts
@@ -178,25 +178,19 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectInput class
-class ProjectInput {
-    templateElement: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-    element: HTMLFormElement;
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
     constructor(){
-        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
-        this.hostElement = document.getElementById("app")! as HTMLDivElement;
-        const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild! as HTMLFormElement;
-        this.element.id = "user-input";
+        super("project-input", "app", true, "user-input")
         this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
-        this.attach();
         this.configure();
     }
+
+    renderContent() {}
 
     private gatherUserInput = (): [string, string, number] | void => {
         const enteredTitle = this.titleInputElement.value;
@@ -251,12 +245,8 @@ class ProjectInput {
         }
     }
 
-    private configure() {
+    configure() {
         this.element.addEventListener("submit", this.handleSubmit);
-    }
-
-    private attach() {
-        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
 
